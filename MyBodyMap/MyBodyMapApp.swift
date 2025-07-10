f070633d56c7abd2f0495b8e011137a6f03c284f
IParamonikhin <paramonikhin.ivan@yandex.ru>
2025-07-10 19:35:46 +0300

    Initial Commit

---
+++ MyBodyMap/MyBodyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBodyMapApp.swift
+//  MyBodyMap
+//
+//  Created by Иван on 10.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct MyBodyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
